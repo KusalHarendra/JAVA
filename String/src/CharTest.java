f1230d08b365aae3c1780e6b3f62c1c745bd45be
Kusal Harendra <85011451+KusalHarendra@users.noreply.github.com>
2023-02-26 16:57:51 +0530

    Add files via upload

---
+++ String/src/CharTest.java
@@ -0,0 +1,8 @@
+public class CharTest {
+    public static void main(String[] args) {
+        char[] c1 = {'J','A','V','A'};      //creating an array
+        String s1 = new String(c1);         //locate that array into s1 variable
+
+        System.out.println(s1);                //print s1
+    }
+}
---
+++ String/src/StringTest2.java
@@ -0,0 +1,15 @@
+public class StringTest2 {
+    public static void main(String[] args) {
+        String s1 = new String("Kusal");
+        String s2 = new String("Kusal");
+
+        if(s1==s2){
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }
+    }
+}
+
+//s1 and s2 have two separate values
