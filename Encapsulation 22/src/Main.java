3a965378f51385dd2cc450454af5b3ce0615088f
Kusal Harendra <85011451+KusalHarendra@users.noreply.github.com>
2023-03-10 16:15:29 +0530

    Add files via upload

---
+++ Encapsulation 22/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Student obj = new Student();
+
+        obj.setName("Kusal");
+        System.out.println(obj.getName());
+
+        obj.setAge(26);
+        System.out.println(obj.getAge());
+
+        }
+}
---
+++ Encapsulation 22/src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String name;
+    private int age;        //private varibales
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+}
